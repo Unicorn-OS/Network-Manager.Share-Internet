b170a4f789c817a01690bc2545269ec9e2fabf32
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-05-31 02:11:46 -0500

    Create dnsmasq.d

---
+++ Configure/IP and Subnet/Solution:/dnsmasq.d
@@ -0,0 +1,4 @@
+# Guide:
+https://fedoramagazine.org/internet-connection-sharing-networkmanager/#:~:text=Changing%20the%20shared%20IP%20range
+
+sch: https://www.google.com/search?q=network+manager+sharing+dnsmasq+set+subnet+ip
